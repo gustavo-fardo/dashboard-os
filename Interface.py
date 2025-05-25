508308333190b3f4d10502f1104c71de2cb94517
Thiago Lucas <thiago.2020@alunos.utfpr.edu.br>
2025-05-25 17:20:50 -0300

    ultimas features - parte A

--- Interface.py
+++ Interface.py
@@ -19,6 +19,7 @@ class Interface:
         self.root.attributes('-zoomed', True)
         self.root.minsize(1000, 1000)
         self.cur_screen = ""
+        self.proc_info_pid = None
 
         self.root.protocol("WM_DELETE_WINDOW", self.on_close)
 
@@ -36,7 +37,7 @@ class Interface:
         self.root.mainloop()
 
     def process_update(self):
-        if not self.process_tree.winfo_exists():
+        if not self.process_tree.winfo_exists() or self.cur_screen != "process":
             return 
         
         existing_items = set(self.process_tree.get_children())
@@ -44,7 +45,6 @@ class Interface:
         
         with self.gerenciador._dictlock:
             proc_dict = dict(sorted(self.gerenciador.getProcDict().items(), key=lambda item: item[1].getCPU(), reverse=True))
-            # proc_dict = self.gerenciador.getProcDict().copy()
 
 
         for pid, process in proc_dict.items():
@@ -54,13 +54,13 @@ class Interface:
                 self.process_tree.item(
                     proc_id, text=proc_id, 
                     values=(process.getNome(), process.getUsuario(), process.getCPU(), process.getMem(), 
-                            process.getEstado(), process.getPrioB(), process.getPrioD())
+                            process.getEstado(), process.getPrioB(), process.getPrioD(), ">>")
                 )
             else:
                 self.process_tree.insert(
                     "", "end", iid=proc_id, text=proc_id, 
                     values=(process.getNome(), process.getUsuario(), process.getCPU(), process.getMem(), 
-                            process.getEstado(), process.getPrioB(), process.getPrioD())
+                            process.getEstado(), process.getPrioB(), process.getPrioD(), ">>")
                 )
 
             thread_ids = set()
@@ -72,14 +72,14 @@ class Interface:
                         proc_id, "end", iid=tid,
                         text=f"{tid}",
                         values=(thread.getNome(), thread.getUsuario(), thread.getCPU(), thread.getMem(), 
-                                thread.getEstado(), thread.getPrioB(), thread.getPrioD())
+                                thread.getEstado(), thread.getPrioB(), thread.getPrioD(), "")
                     )
                 except Exception as e:
                     self.process_tree.item(
                         tid,
                         text=f"{tid}",
                         values=(thread.getNome(), thread.getUsuario(), thread.getCPU(), thread.getMem(), 
-                                thread.getEstado(), thread.getPrioB(), thread.getPrioD())
+                                thread.getEstado(), thread.getPrioB(), thread.getPrioD(), "")
                     )
                     
             for child in self.process_tree.get_children(proc_id):
@@ -107,8 +107,10 @@ class Interface:
         tree_scrollbar.config(command=self.process_tree.yview)
         
         self.process_tree = ttk.Treeview(self.frames["prc"])
+        
+        self.process_tree.bind('<ButtonRelease-1>', self.on_treeview_click)
 
-        self.process_tree["columns"] = ("name", "user", "cpu", "memory", "state", "prioB", "prioD")
+        self.process_tree["columns"] = ("name", "user", "cpu", "memory", "state", "prioB", "prioD", "action")
         self.process_tree.heading("#0", text="PID")
         self.process_tree.heading("name", text="Nome")
         self.process_tree.heading("user", text="Usuário")
@@ -117,6 +119,7 @@ class Interface:
         self.process_tree.heading("state", text="State")
         self.process_tree.heading("prioB", text="PrioD")
         self.process_tree.heading("prioD", text="PrioB")
+        self.process_tree.heading("action", text="Detalhes")
         self.process_tree.column("#0", width=100, anchor="center")
         self.process_tree.column("name", width=240, anchor="center")
         self.process_tree.column("user", width=60, anchor="center")
@@ -125,11 +128,15 @@ class Interface:
         self.process_tree.column("state", width=60, anchor="center")
         self.process_tree.column("prioB", width=60, anchor="center")
         self.process_tree.column("prioD", width=60, anchor="center")
+        self.process_tree.column("action", width=80, anchor="center")
         self.process_tree.pack(fill="both", expand=True)
 
         self.process_update()
 
-    def resources_update(self):        
+    def resources_update(self):
+        if self.cur_screen != "resources":
+            return
+        
         self.meters["cpu"].configure(amounttotal=100, amountused=self.gerenciador._cpuUso)
         self.meters["mem"].configure(amounttotal=self.gerenciador._memTotal, amountused=self.gerenciador._memUso)
         self.meters["memV"].configure(amounttotal=self.gerenciador._memVirtualTotal, amountused=self.gerenciador._memVirtualUso)
@@ -156,6 +163,26 @@ class Interface:
                 self.gerenciador._memVirtualKernelUso
             ], self.gerenciador._memVirtualTotal)
 
+        self.process_info.config(text=f"Processes: {self.gerenciador._numProcessos}")
+        self.threads_info.config(text=f"Threads: {self.gerenciador.getNumThreads()}")
+        
+        self.cpu_info["Uso"].config(text=f"Uso: {self.gerenciador._cpuUso:.2f} %")
+        self.cpu_info["Ociosas"].config(text=f"Ociosas: {self.gerenciador._cpuOcioso:.2f} %")
+        self.cpu_info["Sist."].config(text=f"Sist.: {self.gerenciador._cpuSistema:.2f} %")
+        self.cpu_info["Usuario"].config(text=f"Usuario: {self.gerenciador._cpuUsuario:.2f} %")
+        self.cpu_info["Nice"].config(text=f"Nice: {self.gerenciador._cpuNice:.2f} %")
+        self.cpu_info["Wait"].config(text=f"Wait: {self.gerenciador._cpuWait:.2f} %")
+        self.cpu_info["Irq"].config(text=f"Soft Irq: {self.gerenciador._cpuIrq:.2f} %")
+        self.cpu_info["Soft Irq"].config(text=f"Soft Irq: {self.gerenciador._cpuSoftIrq:.2f} %")
+        
+        self.mem_info["Livre"].config(text=f"Livre: {self.gerenciador._memLivre:.2f} MB")
+        self.mem_info["Uso"].config(text=f"Uso: {self.gerenciador._memUso:.2f} MB")
+        self.mem_info["Buffer"].config(text=f"Buffer: {self.gerenciador._memBuffer:.2f} MB")
+        self.mem_info["Cache"].config(text=f"Cache: {self.gerenciador._memCache:.2f} MB")
+        
+        self.memv_info["Uso"].config(text=f"Buffer: {self.gerenciador._memVirtualUso:.2f} MB")
+        self.memv_info["Kernel Uso"].config(text=f"Cache: {self.gerenciador._memVirtualKernelUso:.2f} MB")
+        
         self.root.update()
         self.root.after(UI_UPDATE_TIME_MS, self.resources_update)
 
@@ -173,8 +200,26 @@ class Interface:
         self.frames["memV"].pack(fill="x", padx=10)
         self.meters["memV"].pack(side="left", padx=10)
         
-        cpu_labels = ["Uso",
-            f"Ociosas",
+        self.cpu_proc_frame = ttk.Frame(self.frames["cpu"])
+        self.cpu_proc_frame.pack(side="right", padx=10)
+        
+        self.process_info = ttk.Label(self.cpu_proc_frame, text=f"Processes: {self.gerenciador._numProcessos}")
+        self.process_info.pack(anchor='w')
+        self.threads_info = ttk.Label(self.cpu_proc_frame, text=f"Threads: {self.gerenciador.getNumThreads()}")
+        self.threads_info.pack(anchor='w')
+        
+        self.mem_info_frame = ttk.Frame(self.frames["mem"])
+        self.mem_info_frame.pack(side="left", padx=10)
+        
+        self.memv_info_frame = ttk.Frame(self.frames["memV"])
+        self.memv_info_frame.pack(side="left", padx=10)
+        
+        self.cpu_info_frame = ttk.Frame(self.frames["cpu"])
+        self.cpu_info_frame.pack(side="left", padx=10)
+        
+        self.cpu_labels = [
+            "Uso",
+            "Ociosas",
             "Sist.",
             "Usuario",
             "Nice",
@@ -183,28 +228,47 @@ class Interface:
             "Soft Irq"
         ]
 
-        mem_labels = [
+        self.mem_labels = [
             "Livre",
             "Uso",
             "Buffer",
             "Cache"
         ]
         
-        memV_labels = [
+        self.memV_labels = [
             "Uso",
             "Kernel Uso"
         ]
+        
+        self.mem_info = {}
+        for l in self.mem_labels:
+            self.mem_info[l] = ttk.Label(self.mem_info_frame, text=f"{l}: -")
+            self.mem_info[l].pack(anchor='w')
+        
+        self.cpu_info = {}
+        for l in self.cpu_labels:
+            self.cpu_info[l] = ttk.Label(self.cpu_info_frame, text=f"{l}: -")
+            self.cpu_info[l].pack(anchor='w')
+        
+        self.memv_info = {}
+        for l in self.memV_labels:
+            self.memv_info[l] = ttk.Label(self.memv_info_frame, text=f"{l}: -")
+            self.memv_info[l].pack(anchor='w')
 
-        self.cpu_chart_frame = AreaChartFrame(self.frames["cpu"], "CPU Usage (%)", 8, cpu_labels)
-        self.mem_chart_frame = AreaChartFrame(self.frames["mem"], "Memory Usage (MB)", 4, mem_labels)
-        self.memV_chart_frame = AreaChartFrame(self.frames["memV"], "Virtual Memory Usage (MB)", 2, memV_labels)
+
+        self.cpu_chart_frame = AreaChartFrame(self.frames["cpu"], "CPU Usage (%)", 8, self.cpu_labels)
+        self.mem_chart_frame = AreaChartFrame(self.frames["mem"], "Memory Usage (MB)", 4, self.mem_labels)
+        self.memV_chart_frame = AreaChartFrame(self.frames["memV"], "Virtual Memory Usage (MB)", 2, self.memV_labels)
 
         self.resources_update()
-
 
     def redraw_processes(self):
         self.clear_widgets()
         self.process_draw()
+
+    def redraw_proc_info(self):
+        self.clear_widgets()
+        self.draw_proc_info()
 
     def redraw_resources(self):
         self.clear_widgets()
@@ -218,7 +282,8 @@ class Interface:
             "cpu": LabelFrame(self.root, text="CPU"), 
             "mem": LabelFrame(self.root, text="Memory"), 
             "memV": LabelFrame(self.root, text="Virtual Memory"),
-            "prc": LabelFrame(self.root, text="Processes", padding=20)
+            "prc": LabelFrame(self.root, text="Processes", padding=20),
+            "prc_info": LabelFrame(self.root, text="Process Info", padding=20)
         }
 
         self.meters = {
@@ -238,13 +303,86 @@ class Interface:
     
     def atualiza_thread_func(self):
         while self.atualiza_thread_running:
-            if self.cur_screen == "resources":
-                self.gerenciador._atualizaMemInfo()
-                self.gerenciador._atualizaCPUInfo()
-            else:
-                self.gerenciador._atualizaProcDict()
+            self.gerenciador.atualizaDados()
             time.sleep(DATA_UPDATE_TIME_S)
             
+    def on_treeview_click(self, event):
+        region = self.process_tree.identify_region(event.x, event.y)
+        
+        if region == "cell":
+            column = self.process_tree.identify_column(event.x)
+            item = self.process_tree.identify_row(event.y)
+            
+            if item:
+                col_name = self.process_tree.heading(column)["text"]
+                text = self.process_tree.item(item, 'text')
+                
+                if not "(TID)" in text:
+                    if col_name == "Detalhes":
+                        self.proc_info_pid = text
+                        self.redraw_proc_info()
+
+    def draw_proc_info(self):
+        self.cur_screen = "proc_info"
+        process_button = ttk.Button(self.root, text="View Processes", command=self.redraw_processes)
+        process_button.pack(pady=10)
+
+        self.frames["prc_info"].pack(fill="both", expand=True, padx=10, pady=5)
+
+        self.proc_labels = [
+            "id", "nome", "usuario", "cpuUso", "memUso", 
+            "numThreads", "memVirtualUso", 
+            "estado", "prioB", "prioD",
+            "memSegments"
+        ]
+        
+        self.proc_info = {}
+            
+        self.proc_info["id"] = ttk.Label(self.frames["prc_info"])
+        self.proc_info["nome"] = ttk.Label(self.frames["prc_info"])
+        self.proc_info["usuario"] = ttk.Label(self.frames["prc_info"])
+        self.proc_info["cpuUso"] = ttk.Label(self.frames["prc_info"])
+        self.proc_info["memUso"] = ttk.Label(self.frames["prc_info"])
+        
+        self.proc_info["numThreads"] = ttk.Label(self.frames["prc_info"])
+        self.proc_info["memVirtualUso"] = ttk.Label(self.frames["prc_info"])
+        
+        self.proc_info["estado"] = ttk.Label(self.frames["prc_info"])
+        self.proc_info["prioB"] = ttk.Label(self.frames["prc_info"])
+        self.proc_info["prioD"] = ttk.Label(self.frames["prc_info"])
+        
+        self.proc_info["memSegments"] = ttk.Label(self.frames["prc_info"])
+        
+        for l in self.proc_labels:
+            self.proc_info[l].pack(anchor='w')
+
+        self.proc_info_update()
+            
+    def proc_info_update(self):
+        if self.cur_screen != "proc_info":
+            return 
+        
+        proc = self.gerenciador.getProcDict()[int(self.proc_info_pid)]
+        
+        mem_seg_text = ""
+        for seg_key, seg_val in proc.getMemSegments().items():
+            mem_seg_text += f"        {seg_key}\n                Page: {seg_val['pages']}\n                Size: {seg_val['size_kb']} KB\n"
+        
+        self.proc_info["id"].config(text=f"PID: {proc._id}")
+        self.proc_info["nome"].config(text=f"Name: {proc._nome}")
+        self.proc_info["usuario"].config(text=f"User: {proc._usuario}")
+        self.proc_info["cpuUso"].config(text=f"CPU Use: {proc._cpuUso:.2f}%")
+        self.proc_info["memUso"].config(text=f"RAM Use: {proc._memUso}")
+        
+        self.proc_info["numThreads"].config(text=f"Threads: {proc._numThreads}")
+        self.proc_info["memVirtualUso"].config(text=f"VRAM: {proc._memVirtualUso} MB")
+        
+        self.proc_info["estado"].config(text=f"State: {proc._estado}")
+        self.proc_info["prioB"].config(text=f"prioB: {proc._prioB}")
+        self.proc_info["prioD"].config(text=f"prioD: {proc._prioD}")
+        
+        self.proc_info["memSegments"].config(text=f"Memory Segments:\n{mem_seg_text}")
+
 
 if __name__ == "__main__":
     app = Interface()
